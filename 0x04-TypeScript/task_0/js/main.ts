48af0492565486fbf0531d22d1ee33ee3de8372c
Enoch Okine Mensah <okaycode100@gmail.com>
2025-09-14 20:40:49 +0000

    Creating an interface for a student

---
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -0,0 +1,41 @@
+interface Student {
+  firstName: string;
+  lastName: string;
+  age: number;
+  location: string;
+}
+const student1: Student = {
+    firstName: 'John',
+    lastName: 'Doe',
+    age: 20,
+    location: 'Accra',
+};
+const student2: Student = {
+    firstName: 'James',
+    lastName: 'Ameh',
+    age: 22,
+    location: 'Kumasi',
+};
+const studentsList: Student[] = [student1, student2];
+
+const table = document.createElement('table');
+const headerRow = document.createElement('tr');
+const headers = ['First Name', 'Location'];
+
+headers.forEach(headerText => {
+    const header = document.createElement('th');
+    header.textContent = headerText;
+    headerRow.appendChild(header);
+});
+table.appendChild(headerRow);
+studentsList.forEach(student => {
+    const row = document.createElement('tr');
+    const firstNameCell = document.createElement('td');
+    firstNameCell.textContent = student.firstName;
+    const locationCell = document.createElement('td');
+    locationCell.textContent = student.location;
+    row.appendChild(firstNameCell);
+    row.appendChild(locationCell);
+    table.appendChild(row);
+});
+document.body.appendChild(table);
